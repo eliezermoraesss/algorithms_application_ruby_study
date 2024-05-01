63314612de2328a4e993061fd14047857678c715
Eliezer <eliezer.moraes@outlook.com>
2024-05-01 15:25:55 -0300

    Building calculator

---
+++ applications/calculator.rb
@@ -0,0 +1,16 @@
+def add(number1,number2)
+    number1 + number2
+end
+
+def sub
+
+print "Digite o primeiro número: "
+number1 = gets.chomp.to_i
+
+print "Digite a operação desejada [+ - * /]"
+operation = gets.chomp
+
+print "Digite o segundo número: "
+number2 = gets.chomp.to_i
+
+puts "O resultado da "
