c2a7c3c0abe555fc2bc1afeee3ce183073d9a0e8
Eliezer <eliezer.moraes@outlook.com>
2024-05-05 16:49:58 -0300

    Created new RUBY Application - Calculator

--- applications/calculator.rb
+++ applications/calculator.rb
@@ -1,17 +1,57 @@
-def add(number1,number2)
+def add(number1, number2)
     number1 + number2
 end
 
-def sub()
+def sub(number1, number2)
+    number1 - number2
 end
 
-print "Digite o primeiro número: "
-number1 = gets.chomp.to_i
+def mult(number1, number2)
+    number1 * number2
+end
 
-print "Digite a operação desejada [+ - * /]"
-operation = gets.chomp
+def div(number1, number2)
+    number1 / number2
+end
 
-print "Digite o segundo número: "
-number2 = gets.chomp.to_i
+loop do
+    puts "Calculadora"
+    puts "Digite uma das opções abaixo: \n"
+    puts "1 - Soma"
+    puts "2 - Subtração"
+    puts "3 - Multiplicação"
+    puts "4 - Divisão"
+    puts "0 - Sair"
+    print 'Opção: '
 
-puts "O resultado da"
+    option = gets.chomp.to_i
+
+    if option == 0
+        break
+    elsif option.between?(1, 4)
+        print "Digite o primeiro número: "
+        number1 = gets.chomp.to_f
+
+        print "Digite o segundo número: "
+        number2 = gets.chomp.to_f
+
+        result = case option
+        when 1
+            add(number1, number2)
+        when 2
+            sub(number1, number2)
+        when 3
+            mult(number1, number2)
+        when 4
+            if number2 == 0
+                puts "Não é possível dividir por 0"
+                next
+            else
+                div(number1, number2)
+            end
+        end
+        puts "O resultado é #{result}\n"
+    else
+        puts "Opção inválida"
+    end
+end
